e0d1894e6f5ae76b279e03c095224a6bba056bc3
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2025-09-16 23:14:54 +0700

    Thêm các controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     */
+    public function index(): View
+    {
+        return view('home'); // hoặc 'home' nếu file view ở resources/views/home.blade.php
+    }
+}
